87eb87e6945b3f624a7d127980d7e13b2e999071
root <root@localhost.localdomain>
2025-12-13 23:47:35 +0000

    KernelSU Next compability fix

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,5 +1,9 @@
-MODPATH=/data/adb/modules/systemui_vayu_overlay
+MODPATH=/data/adb/modules/Vayu_Fix_Overlay
 
 set_perm_recursive $MODPATH/system/vendor/overlay 0 0 0755 0644
 
-touch $MODPATH/system/vendor/overlay/SystemUiOverlayVayu.apk
+touch $MODPATH/system/vendor/overlay/SystemUIOverlayVayu.apk
+
+if [ -n "$KSU" ]; then
+  /system/bin/cmd overlay enable Vayu_Fix_Overlay com.android.systemui 2>/dev/null
+fi
